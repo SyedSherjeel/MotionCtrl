94b08d3b952488e99c5417edb69b833948e31d68
SyedSherjeel <sherjeel.hashmi@vyro.ai>
2024-02-19 11:42:38 +0000

    fix the package installation

--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='motionctrl',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True
+    version='1.1',
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=find_packages(where='src', include=['motionctrl', 'motionctrl.*']),
+    package_data={'': ['*.txt', '*.md', '*.gif', '*.mp4', '*.py', '*.jpg', '*.png'],},
+    # package_data={'motionctrl': ['assets/*', 'configs/*', 'examples/*', 'gradio_utils/*', 'lvdm/*', 'main/*', 'motionctrl/*', 'utils/*']},
+    author='Tencent',
+    description='A Python package for motion control and object tracking',
+    license='MIT',
+    keywords='motion control tracking',
+    url='https://github.com/SyedSherjeel/motionctrl',
 )
